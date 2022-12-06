a68e9313975f7e40f9784e2548db4574b5e505e9
FoxCraft <107763130+FoxCraft67@users.noreply.github.com>
2022-12-06 16:18:13 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@ let myImage = document.querySelector('img');
 myImage.addEventListener('click', function() {
     let mySrc = myImage.getAttribute('src');
     if (mySrc === 'images/logo.png') {
-      myImage.setAttribute('src', 'images/Salut.png');
+      myImage.setAttribute('src', 'images/salut.png');
     } else {
       myImage.setAttribute('src', 'images/logo.png');
     }
